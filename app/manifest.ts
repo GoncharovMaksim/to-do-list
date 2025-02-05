6804d6db807076dfa4ee3f18b1d533c6bd152040
Max112.87 <max112.87>
2025-02-05 14:05:28 +0700

    изменен фавикон

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+	return {
+		name: 'Заметки112',
+		short_name: 'Заметки112',
+		description: 'Приложение для создания списка планируемых дел и задач',
+		start_url: '/',
+		display: 'standalone',
+		background_color: '#ffffff',
+		theme_color: '#000000',
+		icons: [
+			{
+				src: '/icon-192x192.png',
+				sizes: '192x192',
+				type: 'image/png',
+			},
+			{
+				src: '/icon-512x512.png',
+				sizes: '512x512',
+				type: 'image/png',
+			},
+		],
+	};
+}
