24ae6946fe2fd20e565d7a3b1344bbaccf945655
Max112.87 <max112.87>
2025-02-06 18:36:50 +0700

    изменен лого

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,7 +12,7 @@ export default function manifest(): MetadataRoute.Manifest {
 		icons: [
 			{
 				src: '/icon-192x192.png',
-				sizes: '192x192',
+				sizes: '144x144',
 				type: 'image/png',
 			},
 			{
